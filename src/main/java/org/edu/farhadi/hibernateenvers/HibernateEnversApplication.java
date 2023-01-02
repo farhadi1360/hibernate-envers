85ee8ea127084f433332d38490c9f419ab004e51
farhadi1360 <farhadi.kam@gmail.com>
2023-01-02 16:32:24 +0330

    this is base line

---
+++ src/main/java/org/edu/farhadi/hibernateenvers/HibernateEnversApplication.java
@@ -0,0 +1,13 @@
+package org.edu.farhadi.hibernateenvers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateEnversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateEnversApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/edu/farhadi/hibernateenvers/HibernateEnversApplicationTests.java
@@ -0,0 +1,13 @@
+package org.edu.farhadi.hibernateenvers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateEnversApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
